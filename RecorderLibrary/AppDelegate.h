fea5bf1b8dc1ffdb479805d1308e0027cd68e10a
Sumit Gera <gerasumit.dce@gmail.com>
2015-07-03 20:32:52 +0530

    Initial Commit

---
+++ RecorderLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecorderLibrary
+//
+//  Created by Sumit Gera on 03/07/15.
+//  Copyright (c) 2015 SumitGera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecorderLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecorderLibrary
+//
+//  Created by Sumit Gera on 03/07/15.
+//  Copyright (c) 2015 SumitGera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
